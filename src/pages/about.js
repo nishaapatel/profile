b12ffc58626d36c844dfd0382c6150f7ddc40293
Nisha <nishaapatel196@gmail.com>
2025-11-15 10:10:54 +1030

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -105,15 +105,15 @@ const about = () => {
                 </h2>
               </div>
 
-              //<div className="flex flex-col items-end justify-center xl:items-center">
-                /*<span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl                ">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl                ">
                   <AnimatedNumber value={3} />+
                 </span>
                 <h2 className="text-x1 font-medium capitalize text-dark/75 dark:text-light
                 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Years of experience
                 </h2>
-              </div>*/
+              </div>
             </div>
           </div>
           <Skills />
